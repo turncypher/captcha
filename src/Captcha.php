6d297663d444971710754676ad5e6070382b3671
Vance Chang <turncypher@gmail.com>
2018-01-05 13:48:49 +0800

    captcha验证

--- src/Captcha.php
+++ src/Captcha.php
@@ -8,9 +8,10 @@
 
 namespace NovaStar\Captcha;
 
+use Intervention\Image\ImageManager;
+
 class Captcha
 {
-
     public function getCaptcha()
     {
         $types = [SecureImage::SI_CAPTCHA_WORDS, SecureImage::SI_CAPTCHA_MATHEMATIC];
@@ -19,11 +20,23 @@ class Captcha
         $img->ttf_file = __DIR__ . '/../assets/AHGBold.ttf';
         $img->wordlist_file = __DIR__ . '/../assets/words/words.txt';
         $img->captcha_type = mt_rand(1, 2);
-        $img->captcha_type = SecureImage::SI_CAPTCHA_WORDS;
+        $img->captcha_type = SecureImage::SI_CAPTCHA_MATHEMATIC;
         $img->perturbation = 0.5;
         $img->image_width = 230;
         $img->image_height = 60;
         $img->num_lines = mt_rand(3, 4);
-        return $img->show( 'backgrounds/bg4.jpg');
+        $img->session_name = 'sms';
+        $res = $img->show('backgrounds/bg4.jpg');
+
+        $manager = new ImageManager(array('driver' => 'gd'));
+        $image = $manager->make($res);
+        echo $image->response();
+    }
+
+    public function check($captcha)
+    {
+        $img = new SecureImage();
+        $img->session_name = 'sms';
+        return $img->check($captcha);
     }
 }
