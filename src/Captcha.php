5f344713c1d7eb5273769cdc3995619f52a3c80e
Vance Chang <turncypher@gmail.com>
2018-01-05 18:43:26 +0800

    asset paths

--- src/Captcha.php
+++ src/Captcha.php
@@ -14,6 +14,11 @@ class Captcha
 {
     private $config;
 
+    private $captchaTypes = [
+        SecureImage::SI_CAPTCHA_MATHEMATIC,
+        SecureImage::SI_CAPTCHA_WORDS,
+    ];
+
     /**
      * Captcha constructor.
      * @param array $config
@@ -21,23 +26,27 @@ class Captcha
     public function __construct(array $config)
     {
         $config = [
-            'ttf_file' => __DIR__ . '/../assets/AHGBold.ttf',
-            'wordlist_file' => __DIR__ . '/../assets/words/words.txt',
-            'perturbation'  => mt_rand(1,2),
+            'ttf_file' => 'AHGBold.ttf',
+            'wordlist_file' => 'words.txt',
+            'perturbation'  => 0.5,
             'image_width'  => 230,
             'image_height'  => 60,
             'num_lines'  => 3,
-            'background' => __DIR__ . '/../assets/backgrounds/bg4.jpg'
+            'background' => 'bg4.jpg',
+            'captcha_type' => 'random'
         ];
+        if ($config['captcha_type'] == 'random') {
+            $config['captcha_type'] = $this->captchaTypes[array_rand($this->captchaTypes)];
+        }
+        $config['ttf_file'] = __DIR__ . '/../assets/' . $config['ttf_file'];
+        $config['wordlist_file'] = __DIR__ . '/../assets/words/' . $config['wordlist_file'];
+        $config['background'] = __DIR__ . '/../assets/backgrounds/' . $config['background'];
         $this->config = $config;
     }
 
     public function getCaptcha()
     {
-        $types = [SecureImage::SI_CAPTCHA_MATHEMATIC, SecureImage::SI_CAPTCHA_WORDS];
-
         $img = new SecureImage($this->config);
-
         $res = $img->show($this->config['background']);
         $manager = new ImageManager(array('driver' => 'gd'));
         $image = $manager->make($res);
